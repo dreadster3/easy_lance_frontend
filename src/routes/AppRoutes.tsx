793450fbdd608c5b7f020bf0f2fa9b47e19bdc0b
dreadster3 <afonso.antunes@live.com.pt>
2023-08-06 13:59:07 +0100

    Remove debug logs

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,19 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "../app/home/Home";
-import { useAuth } from "../providers/AuthProvider";
-import PrivateRoutes from "./PrivateRoutes";
-import PublicRoutes from "./PublicRoutes";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useAuth } from '../providers/AuthProvider';
+import PrivateRoutes from './PrivateRoutes';
+import PublicRoutes from './PublicRoutes';
 
 function AppRoutes() {
-  const { isAuthenticated } = useAuth();
+    const { isAuthenticated } = useAuth();
 
-  console.log("isAuthenticated: ", isAuthenticated);
-
-  return (
-    <BrowserRouter>
-      {isAuthenticated ? <PrivateRoutes /> : <PublicRoutes />}
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            {isAuthenticated ? <PrivateRoutes /> : <PublicRoutes />}
+        </BrowserRouter>
+    );
 }
 
 export default AppRoutes;
